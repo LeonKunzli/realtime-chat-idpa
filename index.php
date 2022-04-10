d7916e8d3db017d221fd54c6975be95397dc0a1f
Léon Künzli <kuenzlil@bzz.ch>
2022-04-10 13:16:16 +0200

    i think i broke something in config.php but its broken even more differently now and also index is broken now

--- index.php
+++ index.php
@@ -3,19 +3,10 @@ require_once __DIR__ . '/config.php';
 class API {
     function Select(){
         $db = new Connect;
-        $users = array();
-        $data = $db->prepare('SELECT * FROM chatuser ORDER BY user_id');
+        $data = $db->prepare('SHOW TABLES');
         $data->execute();
-        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
-            $users[$OutputData['user_id']] = array(
-                    'user_id' => $OutputData['user_id'],
-                    'username' => $OutputData['username'],
-                    'email' => $OutputData['email'],
-                    'password' => $OutputData['password'],
-                    'role_id' => $OutputData['role_id']
-            );
-        }
-        return json_encode($users);
+        $OutputData = $data->fetch(PDO::FETCH_ASSOC);
+        return json_encode($OutputData);
     }
 }
 
