0b34036e2585a194df8085f34f6f168a93f9d63f
Léon Künzli <kuenzlil@bzz.ch>
2022-04-10 12:15:43 +0200

    i think i broke something in config.php

--- config.php
+++ config.php
@@ -3,7 +3,7 @@ class Connect extends PDO
 {
     public function __construct()
     {
-        parent::__construct("mysql:host=localhost;dbname=realtimechat;port=3307", 'root', 'b77ITm0d',
+        parent::__construct("mysql:host=eu-cdbr-west-02.cleardb.net;dbname=realtimechat;port=3307", 'bbecfd188511a0', '5579d5ad',
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
