5452f2a1eba78b0ac7617e31d433ba032b0d12f3
Léon Künzli <kuenzlil@bzz.ch>
2022-04-10 13:18:23 +0200

    danke elia

--- index.php
+++ index.php
@@ -3,10 +3,19 @@ require_once __DIR__ . '/config.php';
 class API {
     function Select(){
         $db = new Connect;
+        $users = array();
         $data = $db->prepare('SHOW TABLES');
         $data->execute();
-        $OutputData = $data->fetch(PDO::FETCH_ASSOC);
-        return json_encode($OutputData);
+        while($OutputData = $data->fetch(PDO::FETCH_ASSOC)){
+            $users[$OutputData['user_id']] = array(
+                    'user_id' => $OutputData['user_id'],
+                    'username' => $OutputData['username'],
+                    'email' => $OutputData['email'],
+                    'password' => $OutputData['password'],
+                    'role_id' => $OutputData['role_id']
+            );
+        }
+        return json_encode($users);
     }
 }
 
