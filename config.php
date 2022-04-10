12f2cc8c4a9489c5b56989d5d858762ea1e7f340
Léon Künzli <kuenzlil@bzz.ch>
2022-04-10 12:27:47 +0200

    i think i broke something in config.php but its broken differently now

--- config.php
+++ config.php
@@ -3,7 +3,14 @@ class Connect extends PDO
 {
     public function __construct()
     {
-        parent::__construct("mysql:host=eu-cdbr-west-02.cleardb.net;dbname=realtimechat;port=3307", 'bbecfd188511a0', '5579d5ad',
+        $cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+        $cleardb_server = $cleardb_url["host"];
+        $cleardb_username = $cleardb_url["user"];
+        $cleardb_password = $cleardb_url["pass"];
+        $cleardb_db = substr($cleardb_url["path"],1);
+        $active_group = 'default';
+        $query_builder = TRUE;
+        parent::__construct("mysql:host=" . $cleardb_server . ";dbname=" . $cleardb_db, $cleardb_username, $cleardb_password,
         array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
         $this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         $this->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
